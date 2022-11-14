68eaf256d6f734648d7449aaba525f7b4ed34e9a
martacamacho <martacamachovargas@gmail.com>
2022-11-14 12:03:47 +0100

    created main layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 
-const Layout = ({children}) => {
+const Layout = ({title, children}) => {
 
   return (
-    <div>
+    <>
         <Head>
-            <title>Glutenfree shop</title>
+            <title>{title? title + '| Glutenfree shop' : 'Glutenfree shop'}</title>
+            <meta name="description" content="Gluten free Ecommerce Website" />
+            <link rel="icon" href="/favicon.ico" />
         </Head>
-        {children}
-        <footer>
-        </footer>
-    </div>
+        <div className='flex min-h-screen flex-col justify-between'>
+            <header>
+                <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+                    <Link href="/" className="text-lg font-bold">
+                    Glutenfree shop
+                    </Link>
+                    <div>
+                        <Link href="/cart" className='p-2'>Cart</Link>
+                        <Link href="/login" className='p-2'>Login</Link>
+                    </div>
+                </nav>
+            </header>
+            <main className='container m-auto mt-4 px-4'>
+                {children}
+            </main>
+            <footer className='flex h-10 justify-center items-center shadow-inner'>
+            Footer
+            </footer>
+        </div>
+    </>
   )
 }
 
